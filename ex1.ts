f6b87fd28a77a121baca5b580d6b8dde5a9d4c19
RazorSh4rk <iamrazorshark@gmail.com>
2024-07-01 14:55:04 -0500

    fix some small issues

--- ex1.ts
+++ ex1.ts
@@ -2,12 +2,12 @@ class Meditation {
   constructor(public times: number) { }
 
   start() {
-    for (let i = 0; i < this.times; i++) {
+    for (let i = 1; i <= this.times; i++) {
       setTimeout(() => {
         console.log(i);
-      }, this.times - i);
+      }, (this.times - i) * 1000);
     }
-    setTimeout(() => { console.log("Jay Guru Dev"); }, this.times + 1);
+    setTimeout(() => { console.log("Jay Guru Dev"); }, (this.times + 1) * 1000);
   }
 }
 
--- index.ts
+++ index.ts
@@ -1,22 +1,33 @@
 import Meditation from "./ex1";
 import isPrime from "./ex2";
-import recipes from "./ex3"
+import recipes from "./ex3";
 
-const morning_meditation = new Meditation(5);
-morning_meditation.start();
-console.log(`Start meditation`);
+const settings = {
+    x1: false,
+    x2: true,
+    x3: false
+};
 
-console.log('start');
-isPrime(7)
-    .then(console.log)
-    .catch(console.error);
-console.log('end');
+if (settings.x1) {
+    const morning_meditation = new Meditation(5);
+    morning_meditation.start();
+    console.log(`Start meditation`);
+}
+if (settings.x2) {
+    console.log('start');
+    isPrime(7)
+        .then(console.log)
+        .catch(console.error);
+    console.log('end');
 
-try {
-    const p: { prime: boolean; } = await isPrime(8);
-    console.log("prime");
-} catch (err) {
-    console.log("not prime");
+    try {
+        const p: { prime: boolean; } = await isPrime(8);
+        console.log("prime");
+    } catch (err) {
+        console.log("not prime");
+    }
 }
 
-recipes()
+if (settings.x3) {
+    recipes();
+}
